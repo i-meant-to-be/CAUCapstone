fd0b56eef02dd4683d18294cbfb10d38071b0112
leaderkys <leaderkys@naver.com>
2023-09-22 20:31:32 +0900

    003. python-kotlin integration test code

---
+++ image_processing/integration_test.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2 as cv
+#import matplotlib
+#matplotlib.use('TkAgg')
+#import matplotlib.pyplot as plt
+
+def test(img):
+	img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+	img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+	result = cv.resize(img_gray, None, fx=0.5, fy=0.5, interpolation=cv.INTER_AREA)
+	result = cv.cvtColor(result, cv.COLOR_BGR2RGB)
+	return result
+
+'''
+if __name__ == '__main__':
+	img = cv.imread('jellybeannn.png')
+	img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+	plt.imshow(test(img))
+	plt.show()
+'''

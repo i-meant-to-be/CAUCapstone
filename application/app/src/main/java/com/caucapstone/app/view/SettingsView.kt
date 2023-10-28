18389ef87dbd552df7a19f3c310ce37b65bdbb70
Shawn Kang <shineksu813@outlook.com>
2023-10-28 15:19:39 +0900

    FE010. 설정 컴포너블 작업

--- application/app/src/main/java/com/caucapstone/app/view/SettingsView.kt
+++ application/app/src/main/java/com/caucapstone/app/view/SettingsView.kt
@@ -3,6 +3,7 @@ package com.caucapstone.app.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,15 +11,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -29,59 +34,85 @@ import com.caucapstone.app.data.globalPaddingValue
 
 @Composable
 fun SettingView() {
-    val checked = remember { mutableStateOf(false) }
-
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .padding(globalPaddingValue)
     ) {
-        items(10) { item ->
-            TextButton(
-                modifier = Modifier.fillMaxWidth(),
-                onClick = { /*TODO*/ }
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(imageVector = Icons.Filled.Check, contentDescription = null)
-                    Box(modifier = Modifier.width(10.dp))
-                    Text("Setting menu $item")
-                }
-            }
-            TextButton(
-                modifier = Modifier.fillMaxWidth(),
-                enabled = false,
-                onClick = { /*TODO*/ }
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Icon(imageVector = Icons.Filled.Check, contentDescription = null)
-                        Box(modifier = Modifier.width(10.dp))
-                        Text("Setting menu $item")
-                    }
-                    Switch(
-                        checked = checked.value,
-                        onCheckedChange = {checked.value = !checked.value},
-                        thumbContent = {
-                            if (checked.value) {
-                                Icon(
-                                    imageVector = Icons.Filled.Check,
-                                    contentDescription = null,
-                                    modifier = Modifier.size(SwitchDefaults.IconSize),
-                                )
-                            } else {
-                                null
-                            }
-                        }
+        items(5) { item ->
+            SwitchSettingViewItem("설정 항목 $item", "패턴 필터의 투명도를 조정합니다.", item == 5)
+        }
+        items(5) { item ->
+            TextFieldSettingViewItem("설정 항목 $item", "패턴 필터의 투명도를 조정합니다.", item == 5)
+        }
+    }
+}
+
+@Composable
+fun SwitchSettingViewItem(
+    itemName: String,
+    itemDescription: String = "",
+    isLastItem: Boolean = false
+) {
+    val checked = remember { mutableStateOf(false) }
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = if (isLastItem) 0.dp else 20.dp)
+    ) {
+
+        Column() {
+            Text(itemName, style = MaterialTheme.typography.titleLarge)
+            Text(itemDescription, style = MaterialTheme.typography.bodyMedium)
+        }
+        Switch(
+            checked = checked.value,
+            onCheckedChange = { checked.value = !checked.value },
+            thumbContent = if (checked.value) {
+                {
+                    Icon(
+                        imageVector = Icons.Filled.Check,
+                        contentDescription = null,
+                        modifier = Modifier.size(SwitchDefaults.IconSize)
                     )
                 }
+            } else {
+                null
             }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextFieldSettingViewItem(
+    itemName: String,
+    itemDescription: String = "",
+    isLastItem: Boolean = false
+) {
+    val text = remember { mutableStateOf("") }
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = if (isLastItem) 0.dp else 20.dp)
+    ) {
+
+        Column() {
+            Text(itemName, style = MaterialTheme.typography.titleLarge)
+            Text(itemDescription, style = MaterialTheme.typography.bodyMedium)
         }
+        OutlinedTextField(
+            modifier = Modifier.size(width = 50.dp, height = 20.dp),
+            shape = RoundedCornerShape(10.dp),
+            value = text.value,
+            textStyle = MaterialTheme.typography.bodySmall,
+            onValueChange = { text.value = it }
+        )
     }
 }

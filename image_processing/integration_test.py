059e7233ed83977d7215295708947d56ca95b4b9
leaderkys <leaderkys@naver.com>
2023-11-13 11:44:13 +0900

    test() now returns byte string of png-encoded image

--- image_processing/integration_test.py
+++ image_processing/integration_test.py
@@ -9,7 +9,9 @@ def test(img):
 	img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 	result = cv.resize(img_gray, None, fx=0.5, fy=0.5, interpolation=cv.INTER_AREA)
 	result = cv.cvtColor(result, cv.COLOR_BGR2RGB)
-	return result
+	_, b = cv.imencode('.png',result)
+	return b.tobytes()
+	#return result
 
 '''
 if __name__ == '__main__':

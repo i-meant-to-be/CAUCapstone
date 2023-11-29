fe30325d170e234e49f22b8703bbe520dfef7a0b
eojindr <44453643+eojindr@users.noreply.github.com>
2023-11-29 20:52:59 +0900

    RC008.stripe필터투명도반영

--- CameraTest/app/src/main/java/com/caucapstone/cameratest/PreviewAndFilter.kt
+++ CameraTest/app/src/main/java/com/caucapstone/cameratest/PreviewAndFilter.kt
@@ -376,7 +376,6 @@ fun stripeFilter(inputBitmap: Bitmap, blindType : Int): Bitmap {
     val height = inputBitmap.height
     val outputBitmap = Bitmap.createScaledBitmap(inputBitmap, width, height, true)
     val v = 50
-    val stripePoint = Color.rgb(50, 50, 50)
     val criteria = when (blindType){
         TYPE_NUM_PROTANOPIA -> 0
         TYPE_NUM_DEUTERANOPIA -> 120
@@ -392,11 +391,13 @@ fun stripeFilter(inputBitmap: Bitmap, blindType : Int): Bitmap {
             val hue = hsv[0]
             val sat = hsv[1]
 
+            val diffHue = minOf(abs(hue - criteria), hue + 360 - criteria,criteria + 360 - hue)
             // 색각이상별로 잘 안보이는 색상(적색계열, 녹색계열, 청색계열)의 hue값에 검정무늬 넣기
             if ((abs(hue - criteria) <= v || hue + 360 - criteria <= v || criteria + 360 - hue <= v) && sat > 0.30 && (x+y)%19 == 1 ) {
+                val pointColor =Color.argb((255*(1-diffHue/50)).toInt(),0,0,0)
                 try {
-                    outputBitmap.setPixel(x+1, y, stripePoint)
-                    outputBitmap.setPixel(x, y+1, stripePoint)
+                    outputBitmap.setPixel(x+1, y, pointColor)
+                    outputBitmap.setPixel(x, y+1, pointColor)
                 } catch (e : ArrayIndexOutOfBoundsException){}
             }
         }
